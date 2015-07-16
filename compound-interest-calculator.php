7c9e33ef75c56008d68274103b59f5e053531850
AuTiMoThY <m62466@gmail.com>
2015-07-16 15:24:13 +0800

    計算機更換

--- compound-interest-calculator.php
+++ compound-interest-calculator.php
@@ -40,18 +40,18 @@
 <main class="">
 	<section class="row">
 		<div class="wrapper">
-			<div>
-			  <iframe src="http://widgets.calculatestuff.com/?token=cb3c3e7c1401" frameborder="0" width="100%" height="500" scrolling="no" style="border:none;" id="cb3c3e7c1401"></iframe>
-			  <script type="text/javascript">
-			    (function() {
-			      var s = document.createElement("script");
-			      s.type = "text/javascript";
-			      s.async = true;
-			      s.src="http://cdn.calculatestuff.com/resizer.js";
-			      (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(s);
-			    })();
-			   </script>
-			</div>
+<div>
+  <iframe src="http://widgets.calculatestuff.com/?token=f5ba8ec41415" frameborder="0" width="100%" height="500" scrolling="no" style="border:none;" id="f5ba8ec41415"></iframe>
+  <script type="text/javascript">
+    (function() {
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src="http://cdn.calculatestuff.com/resizer.js";
+      (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(s);
+    })();
+   </script>
+</div>
 		</div>
 	</section>
 
